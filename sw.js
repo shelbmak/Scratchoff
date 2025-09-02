a93e555f3a8b7e0898008bd3f46259a7564f31ca
shelbmak <shelbymhoard@gmail.com>
2025-09-01 19:52:57 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'scratch-app-v1';
+const urlsToCache = ['/', '/index.html', '/cover.png', '/art.png', '/manifest.json'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
